3ee9ca719ee5fd98523d05f0604ef18380bbb571
Evandaly27 <evan.daly1127@gmail.com>
2024-04-15 15:31:57 -0600

    updated functions to get weather to display

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     form.addEventListener('submit', function (event) {
         event.preventDefault();
-        const cityName = cityInput.attributeStyleMap();
+        const cityName = cityInput.value.trim();
         if (cityName === '') return;
 
         fetchWeather(cityName);
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 fetch(forecastURL)
             ]);
 
-            if (!currentRespone.of || !forecastResponse.ok) {
+            if (!currentRespone.ok || !forecastResponse.ok) {
                 throw new Error('City not found');
             }
 
@@ -36,12 +36,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
         } catch (error) {
             console.error('Error fetching weather:', error.message);
+            currentWeather.innerHTML = `<p>Error fetching weather data: ${error.message}</p>`
         }
     }
 
     function displayCurrentWeather(data) {
         const cityName = data.name;
-        const temperature = Math.round(data.main.temp - 273.15);
+        const temperature = Math.round(((data.main.temp - 273.15) * 9/5) + 32);
         const humidity = data.main.humidity;
         const windSpeed = data.wind.speed;
         const weatherIcon = data.weather[0].icon;
@@ -49,17 +50,31 @@ document.addEventListener('DOMContentLoaded', function () {
         const html = `
         <h2>${cityName}</h2>
         <img src="http://openweathermap.org/img/w/${weatherIcon}.png" alt="Weather icon">
-        <p>Temperature: ${temperature}°C</p>
+        <p>Temperature: ${temperature}°F</p>
         <p>Humidity: ${humidity}%</p>
         <p>Wind Speed: ${windSpeed} m/s</p>`;
 
         currentWeather.innerHTML = html;
     }
     function displayForecast(data) {
-
+        let html = '<h2>5-Day Forecast:</h2>';
+        const dailyData = data.list.filter((reading, index) => index % 8 === 0);
+        dailyData.forEach((day) => {
+            const date = new Date(day.dt_txt).toLocaleDateString();
+            const temp = Math.round(((day.main.temp - 273.15) *9/5) + 32);
+            const icon = day. weather[0].icon;
+            html += `
+            <div>
+            <h3>${date}</h3>
+            <img src="http://openweathermap.org/img/w/${icon}.png" alt="Weather icon">
+            <p>Temp: ${temp}°F</p>
+        </div>
+        `;
+        });
+        forecastSection.innerHTML = html
     }
 
     function displaySearchHistory(history) {
-        
+
     }
 });
